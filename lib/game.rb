f603464024e8f61a8a568a3bfed0aedeef25a4f6
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-04-11 12:14:23 -0300

    Edit game.rb here it initialize the player checks for the winner in a loop and switch players it has the place marker func too

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,51 @@
+require_relative 'board.rb'
+require_relative 'player.rb'
 
+class Game
+  def initialize(player_1, player_2)
+    @board = Board.new
+    @current_player_id = 0
+    @players = [
+    player_1.new(@board, 'X'),
+    player_2.new(@board, 'O')
+  ]
+  if @players.any?(&:nil?)
+    raise "Player initialization failed!"
+  end
+  end
+
+  def current_player
+    player = @players[@current_player_id]
+    unless player
+      raise "No player found at index #{@current_player_id} in @players array"
+    end
+    player
+  end
+
+  def play
+    loop do
+      @board.print_board
+      puts ''
+      place_player_marker(current_player)
+
+      if @board.winner?(current_player.marker)
+        puts "#{current_player.marker} wins!"
+        return
+      elsif @board.full?
+        puts "It's a draw!"
+        @board.print_board
+        return
+      end
+      switch_players!
+    end
+  end
+
+  def place_player_marker(player)
+    position = player.select_position!
+    @board.mark_position(position, player.marker)
+  end
+
+  def switch_players!
+    @current_player_id = 1 - @current_player_id
+  end
+end
