03cb06f1ff5f43eba872095ed444623a8f818f58
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-04-11 12:15:00 -0300

    Edit main.rb all add all requires and the necessary initialization

--- main.rb
+++ main.rb
@@ -1,9 +1,4 @@
 require_relative 'lib/game.rb'
-require_relative 'lib/board.rb'
 require_relative 'lib/player.rb'
 
-board = Board.new
-board.print_board
-board.mark_position(2, 'X')
-puts ''
-board.print_board
+Game.new(HumanPlayer, ComputerPlayer).play
